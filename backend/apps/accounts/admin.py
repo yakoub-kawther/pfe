4446caabd93b528634ca9f507084687af8c781df
yakoub kawther <kawterk882@gmail.com>
2026-04-19 01:50:33 +0100

    fix acc and add the other functions

--- backend/apps/accounts/admin.py
+++ backend/apps/accounts/admin.py
@@ -1,3 +1,19 @@
-from django.contrib import admin
+# apps/accounts/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from .models import Account, Role
+
+
+@admin.register(Role)
+class RoleAdmin(admin.ModelAdmin):
+    list_display  = ['id', 'name']
+    search_fields = ['name']
+
+
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    list_display   = ['id', 'role', 'status', 'created_at' , 'password_hash']
+    list_filter    = ['role', 'status']
+    search_fields  = ['student__person__first_name',
+                      'employee__person__first_name']
+    readonly_fields = ['created_at']
